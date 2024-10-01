fadb43cd7ee631bae7e39d0ff4ddd4082183bcdc
Arnav Bhushan <arnavhzb07@gmail.com>
2024-10-01 07:30:54 +0530

    Doubly Linked List

--- Day4A.py
+++ Day4A.py
@@ -58,6 +58,43 @@ class DLL:
         while temp is not None:
             print(temp.utem,end=' ')
             temp=temp.next
+    def delete_first(self):
+        if self.start is not None:
+            self.start=self.start.next
+            if self.start is not None:
+                self.start.prev=None
+    def delete_last(self):
+        if self.start==None:
+            pass
+        elif self.start.next is None:
+            self.start=None
+        else:
+            temp=self.start
+            while temp.next is not None:
+                temp=temp.next
+            temp.prev.next=None
+    def delete_item(self,data):
+        if self.start is None:
+            pass
+        else:
+            temp=self.start
+            while temp is not None:
+                    if temp.item==data:
+                        if temp.next is not None:
+                            temp.next.prev=temp.prev
+                        if temp.prev is not None:
+                            temp.prev.next=temp.next
+                        else:
+                            self.start=temp.next
+                        break
+                    temp=temp.next
+                    
+                    
+    
+        
+                
+                
+    
     
     
         
