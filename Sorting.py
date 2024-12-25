e01088673438353b01b6f78c9e7ad6a97cee1005
Arnav Bhushan <arnavhzb07@gmail.com>
2024-12-25 14:38:30 +0530

    SORTING

--- Sorting.py
+++ Sorting.py
@@ -83,3 +83,37 @@ p=[42, 87, 15, 73, 56, 32, 98, 67, 11, 49]
 p=quick_sort(p)
 print("Quick Sort :- ",p)
 print()
+
+
+# MERGE SORT
+def merge_sort(list1):
+    if len(list1)>1:
+        mid=len(list1)//2
+        leftlist=list1[:mid]
+        rightlist=list1[mid:]
+        
+        merge_sort(leftlist)
+        merge_sort(rightlist)
+        
+        i=j=k=0
+        while i<len(leftlist) and j<len(rightlist):
+            if leftlist[i]<rightlist[j]:
+                list1[k]=leftlist[i]
+                i+=1
+            else:
+                list1[k]=rightlist[j]
+                j+=1
+            k+=1
+        while i<len(leftlist):
+            list1[k]=leftlist[i]
+            i+=1
+            k+=1
+        while j<len(rightlist):
+            list1[k]=rightlist[j]
+            j+=1
+            k+=1
+
+q=[42, 87, 15, 73, 56, 32, 98, 67, 11, 49]
+p=merge_sort(q)
+print("Merge Sort :- ",q)
+print()
